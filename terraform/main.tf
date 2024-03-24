cd3fb0784862fb2215077e04bbf53e8de3b83e39
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-03-24 00:28:13 -0700

    another test

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,14 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+# Reference an existing S3 bucket
+data "aws_s3_bucket" "existing" {
   bucket = "cmpe282-first-bucket"
 }
 
+# Example usage: Upload a file to the existing bucket
 resource "aws_s3_object" "readme_file" {
-  bucket = aws_s3_bucket.my_bucket.id
-  key    = "README.md"  # The name that will appear in the bucket
-  source = "README.md"  # Path to the README file in your project directory
+  bucket = data.aws_s3_bucket.existing.bucket
+  key    = "README.md"
+  source = "README.md"
 }
