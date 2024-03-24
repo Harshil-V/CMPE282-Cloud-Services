ca592a842fe1429b535d4976b5f1bff13916bbc5
Harshil Vyas <54691846+Harshil-V@users.noreply.github.com>
2024-03-24 00:21:21 -0700

    testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,12 @@ provider "aws" {
   region = "us-west-1"
 }
 
-# Assuming this bucket is already defined in your configuration
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "cmpe282-first-bucket"
 }
 
-# Upload the README file to the S3 bucket
-resource "aws_s3_bucket_object" "readme_file" {
-  bucket = aws_s3_bucket.my_bucket.bucket
+resource "aws_s3_object" "readme_file" {
+  bucket = aws_s3_bucket.my_bucket.id
   key    = "README.md"  # The name that will appear in the bucket
   source = "README.md"  # Path to the README file in your project directory
 }
